897db39ef26a6007e99d19bc1b512810273ee46c
olivier <olivier@evaletolab.ch>
2018-05-25 11:05:01 +0200

    initial server error page

---
+++ src/app/kng-server-error-found/kng-server-error-found.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kng-server-error-found',
+  templateUrl: './kng-server-error-found.component.html',
+  styleUrls: ['./kng-server-error-found.component.scss']
+})
+export class KngServerErrorFoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
