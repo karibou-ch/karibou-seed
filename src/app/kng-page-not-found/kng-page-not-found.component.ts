9ae3bf7395c512b9eae0c5e8818b267d5028534f
olivier <olivier@evaletolab.ch>
2018-05-25 11:05:23 +0200

    initial page not found

---
+++ src/app/kng-page-not-found/kng-page-not-found.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kng-page-not-found',
+  templateUrl: './kng-page-not-found.component.html',
+  styleUrls: ['./kng-page-not-found.component.scss']
+})
+export class KngPageNotFoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
