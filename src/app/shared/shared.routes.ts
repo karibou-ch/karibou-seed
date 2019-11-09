daa0f75d482a6fe632a13fcc06223b09bd9cde1d
olivier <olivier@evaletolab.ch>
2019-11-09 12:24:58 +0100

    new: initial routing for custom tabs

--- src/app/shared/shared.routes.ts
+++ src/app/shared/shared.routes.ts
@@ -33,8 +33,29 @@ export const appRoutes: Routes = [
   { path: 'shops', loadChildren: '../kng-shops/kng-shops.module#ShopsModule' },
   { path: 'content', loadChildren: '../kng-document/kng-document.module#KngDocumentModule' },      
   { path: 'admin', loadChildren: '../kng-admin/admin.module#AdminModule'  },
-  { path: 'cart',  loadChildren: '../kng-cart/kng-cart.module#KngCartModule'   },
-  { path: 'home', component:KngHomeComponent,resolve:{ loader:LoaderResolve },children:[
+  { path: 'cart', data:{ target:'cart' },  loadChildren: '../kng-cart/kng-cart.module#KngCartModule'   },
+  // FIXME path construction is ugly 
+  { path: 'home/cellar', component:KngHomeComponent, data:{ target:'cellar' }, resolve:{ loader:LoaderResolve },children:[
+    { path: 'products/:sku/:title', component: ProductComponent},
+    { path: 'products/:sku', component: ProductComponent},
+    { path: 'products', pathMatch: 'full', redirectTo: '/store/geneva/home/cellar'  },
+    { path: 'category/:category/:child', component: ProductListComponent},
+    { path: 'category/:category', component: ProductListComponent},
+    { path: 'category', pathMatch:'full', redirectTo:'' },
+    { path: '', pathMatch:'full', redirectTo:''}
+  ]},
+
+  { path: 'home/delicacy', component:KngHomeComponent, data:{ target:'delicacy' }, resolve:{ loader:LoaderResolve },children:[
+    { path: 'products/:sku/:title', component: ProductComponent},
+    { path: 'products/:sku', component: ProductComponent},
+    { path: 'products', pathMatch: 'full', redirectTo: '/store/geneva/home/delicacy'  },
+    { path: 'category/:category/:child', component: ProductListComponent},
+    { path: 'category/:category', component: ProductListComponent},
+    { path: 'category', pathMatch:'full', redirectTo:'' },
+    { path: '', pathMatch:'full', redirectTo:''}
+  ]},
+
+  { path: 'home', component:KngHomeComponent,data:{ target:'home' }, resolve:{ loader:LoaderResolve },children:[
     { path: 'products/:sku/:title', component: ProductComponent},
     { path: 'products/:sku', component: ProductComponent},
     { path: 'products', pathMatch: 'full', redirectTo: '/store/geneva/home'  },
