e9aa156ac751f3c3212ab0676a0c01146d45b2b9
Nathalie Chabert <nathchab@MBP-de-Nathalie.fritz.box>
2019-12-04 18:49:18 +0100

    FIX : code failure shared

--- src/app/shared/kng-ui-bottom-actions/kng-ui-bottom-actions.component.ts
+++ src/app/shared/kng-ui-bottom-actions/kng-ui-bottom-actions.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, ViewEncapsulation, HostBinding, Input, ElementRef, ViewChild, EventEmitter, Output, OnDestroy, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+// tslint:disable-next-line: import-spacing
+import { Component, OnInit, ViewEncapsulation, HostBinding, Input, ElementRef, ViewChild, EventEmitter, Output, OnDestroy, ChangeDetectionStrategy, ChangeDetectorRef }
+from '@angular/core';
 import { Category, ProductService, Product, CartService } from 'kng2-core';
 import { i18n } from '../../common';
 
@@ -11,21 +13,21 @@ import { i18n } from '../../common';
 })
 export class KngUiBottomActionsComponent implements OnInit, OnDestroy {
 
-  @Input() categories:Category[];
-  @Input() exited:boolean;
-  store:string='geneva';
-  show:boolean;
-  findGetNull:boolean;
-  products:Product[]=[];
+  @Input() categories: Category[];
+  @Input() exited: boolean;
+  store = 'geneva';
+  show: boolean;
+  findGetNull: boolean;
+  products: Product[] = [];
 
-  i18n:any={
-    fr:{
-      search_placeholder:"Recherche",
+  i18n: any = {
+    fr: {
+      search_placeholder: 'Recherche',
     },
-    en:{
-      search_placeholder:"Search",
+    en: {
+      search_placeholder: 'Search',
     }
-  }
+  };
 
   @HostBinding('class.show') get classShow(): boolean {
     return this.show;
@@ -35,125 +37,128 @@ export class KngUiBottomActionsComponent implements OnInit, OnDestroy {
     return this.exited;
   }
 
-  @Output() selected:EventEmitter<string>=new EventEmitter<string>();
+  @Output() selected: EventEmitter<string> = new EventEmitter<string>();
 
-  @ViewChild('search') search:ElementRef;
-  @ViewChild('stats') stats:ElementRef;
-  
+  @ViewChild('search') search: ElementRef;
+  @ViewChild('stats') stats: ElementRef;
+
   constructor(
-    public  $i18n:i18n,
+    public  $i18n: i18n,
     private $cart: CartService,
-    private $products:ProductService,
+    private $products: ProductService,
     private cdr: ChangeDetectorRef
   ) { }
 
   ngOnInit() {
-    this.categories=this.categories.sort(this.sortByWeight).filter((c,i)=> {
-      return c.active&&(c.type==='Category');
+    this.categories = this.categories.sort(this.sortByWeight).filter((c, i) => {
+      return c.active && (c.type === 'Category');
     });
   }
 
-  ngAfterContentChecked(){
+    // TOCHECK
+  ngAfterContentChecked() {
   }
 
-  ngOnDestroy(){
+  ngOnDestroy() {
     document.body.classList.remove('mdc-dialog-scroll-lock');
     document.documentElement.classList.remove('mdc-dialog-scroll-lock');
   }
 
-  get locale(){
+  get locale() {
     return this.$i18n.locale;
   }
 
 
-  addToCard(product){
+  addToCard(product) {
     this.$cart.add(new Product(product));
   }
 
-  hasSearch(){
-    return this.search.nativeElement.value
+  hasSearch() {
+    return this.search.nativeElement.value;
   }
 
-  doClear(){
-    this.products=[];
-    this.search.nativeElement.value=null;
-    this.stats.nativeElement.innerText='';
+  doClear() {
+    this.products = [];
+    this.search.nativeElement.value = null;
+    this.stats.nativeElement.innerText = '';
     document.body.classList.add('mdc-dialog-scroll-lock');
     document.documentElement.classList.add('mdc-dialog-scroll-lock');
-    this.cdr.markForCheck();    
+    this.cdr.markForCheck();
   }
 
-  doGoCategory(slug){
+  doGoCategory(slug) {
     this.selected.emit(slug);
     this.doClear();
-    this.show=false;
+    this.show = false;
     document.body.classList.remove('mdc-dialog-scroll-lock');
     document.documentElement.classList.remove('mdc-dialog-scroll-lock');
   }
 
-  doInput(value?:string){
-    let blur=!value;
-    let margin=8; // display stats result
-    value=value||this.search.nativeElement.value;
-    let tokens=value.split(' ').map(val=>(val||'').length);
+  doInput(value?: string) {
+    const blur = !value;
+    let margin = 8; // display stats result
+    value = value || this.search.nativeElement.value;
+    const tokens = value.split(' ').map(val => (val || '').length);
     document.body.classList.add('mdc-dialog-scroll-lock');
     document.documentElement.classList.add('mdc-dialog-scroll-lock');
 
-    this.stats.nativeElement.innerText='';
+    this.stats.nativeElement.innerText = '';
 
     //
     // on search open window
-    if(tokens.some(len=>len>=3)){
-      this.show=true;
-      this.findGetNull=false;
-      margin=(this.search.nativeElement.value||'').length*margin;
+    if (tokens.some(len => len >= 3)) {
+      this.show = true;
+      this.findGetNull = false;
+      margin = (this.search.nativeElement.value || '').length * margin;
 
-      this.$products.search(value).subscribe(products=>{
+      this.$products.search(value).subscribe(products => {
         //
         // async clear?
-        this.stats.nativeElement.style.marginLeft=35+margin+'px';
-        if(!this.search.nativeElement.value){
-          this.stats.nativeElement.innerText='';
+        this.stats.nativeElement.style.marginLeft = 35 + margin + 'px';
+        if (!this.search.nativeElement.value) {
+          this.stats.nativeElement.innerText = '';
           return;
         }
-        this.stats.nativeElement.innerText='('+products.length+')';
+        this.stats.nativeElement.innerText = '(' + products.length + ')';
 
-        this.findGetNull=!products.length
-        this.products=products.sort(this.sortByScore);
-        blur&&this.search.nativeElement.blur();
+        this.findGetNull = !products.length;
+        this.products = products.sort(this.sortByScore);
+        // TOCHECK
+        // unused expression, expected an assignment or function call (no-unused-expression)
+        blur && this.search.nativeElement.blur();
         this.cdr.markForCheck();
       });
     }
   }
 
-  doToggle(){
-    this.show=!this.show;
-    if(this.show){
-      this.products=[];
+  doToggle() {
+    this.show = !this.show;
+    if (this.show) {
+      this.products = [];
       document.body.classList.add('mdc-dialog-scroll-lock');
       document.documentElement.classList.add('mdc-dialog-scroll-lock');
       // this.search.nativeElement.focus();
-    } else{
+    } else {
       this.doClear();
       document.body.classList.remove('mdc-dialog-scroll-lock');
       document.documentElement.classList.remove('mdc-dialog-scroll-lock');
     }
 
   }
-  onFocus(){
-    try{
+  onFocus() {
+    try {
       this.search.nativeElement.select();
-      
-    }catch(e){}
-    
+
+    } catch (e) {}
+
   }
 
-  sortByScore(a,b){
-    return b.stats.score-a.stats.score;
+  sortByScore(a, b) {
+    return b.stats.score - a.stats.score;
   }
 
-  sortByWeight(a:Category,b:Category){
-    return a.weight-b.weight;
+  sortByWeight(a: Category, b: Category) {
+    return a.weight - b.weight;
   }
-    
+
 }
--- src/app/shared/kng-user-reminder/kng-user-reminder.component.ts
+++ src/app/shared/kng-user-reminder/kng-user-reminder.component.ts
@@ -12,103 +12,109 @@ import { MdcSnackbar } from '@angular-mdc/web';
 })
 export class KngUserReminderComponent implements OnInit {
 
-  @Input() user:User;
+  @Input() user: User;
 
-  locale:string;
-  show:boolean;
+  locale: string;
+  show: boolean;
 
 
   //
   // i18n
 
-  i18n:any={
-    fr:{
-      title:"Nous pouvons vous envoyer un mail de rappel afin de vous aider dans l'organisation de vos courses",
-      super:"Pratique!",
-      title_time:"A quelle heure souhaitez vous recevoir la notification",
-      title_day:"A quelles moments souhaitez vous recevoir la notification?"
+  i18n: any = {
+    fr: {
+      title: 'Nous pouvons vous envoyer un mail de rappel afin de vous aider dans l\'organisation de vos courses',
+      super: 'Pratique!',
+      title_time: 'A quelle heure souhaitez vous recevoir la notification',
+      title_day: 'A quelles moments souhaitez vous recevoir la notification?'
     },
-    en:{
-      title:"We can send you a reminder email to help you organize your shopping",
-      super:"Awesome!",
-      title_time:"At what time do you want to receive notification?",
-      title_day:"Which days?"
+    en: {
+      title: 'We can send you a reminder email to help you organize your shopping',
+      super: 'Awesome!',
+      title_time: 'At what time do you want to receive notification?',
+      title_day: 'Which days?'
     }
-  }
+  };
 
-  times=[
-    {value:'8',label:'8h00'},
-    {value:'10',label:'10h00'},
-    {value:'11',label:'11h00'},
-    {value:'14',label:'14h00'},
-    {value:'16',label:'16h00'},
-    {value:'20',label:'20h00'}
+  times = [
+    {value: '8', label: '8h00'},
+    {value: '10', label: '10h00'},
+    {value: '11', label: '11h00'},
+    {value: '14', label: '14h00'},
+    {value: '16', label: '16h00'},
+    {value: '20', label: '20h00'}
   ];
-  weekdays=[
-    {value:'1',label:'Lundi'},
-    {value:'2',label:'Mardi'},
-    {value:'3',label:'Mercredi'},
-    {value:'4',label:'Jeudi'},
-    {value:'5',label:'Vendredi'},
-    {value:'6',label:'Samedi'},
-    {value:'0',label:'Dimanche'}
-  ]
+  weekdays = [
+    {value: '1', label: 'Lundi'},
+    {value: '2', label: 'Mardi'},
+    {value: '3', label: 'Mercredi'},
+    {value: '4', label: 'Jeudi'},
+    {value: '5', label: 'Vendredi'},
+    {value: '6', label: 'Samedi'},
+    {value: '0', label: 'Dimanche'}
+  ];
 
 
   constructor(
-    public  $i18n:i18n,
-    private $user:UserService,
+    public  $i18n: i18n,
+    private $user: UserService,
     private $route: ActivatedRoute,
-    private $snack:MdcSnackbar
-  ) { 
-    let loader=this.$route.snapshot.parent.data['loader']||this.$route.snapshot.data['loader'];
-    if(loader.length){
-      this.user=this.user||loader[1];
+    private $snack: MdcSnackbar
+  ) {
+    const loader = this.$route.snapshot.parent.data['loader'] || this.$route.snapshot.data['loader'];
+    if (loader.length) {
+      this.user = this.user || loader[1];
     }
   }
 
-  doUpdate(event,day:number,time){
-    // FIXME change envent called 2x!    
-    //console.log('MdcCheckboxChange',event.type)
-    if(!event.type){
+  doUpdate(event, day: number, time) {
+    // FIXME change envent called 2x!
+    // console.log('MdcCheckboxChange',event.type)
+    if (!event.type) {
       return;
     }
 
-    if(day){
-      day=day|0;
-      let pos=this.user.reminder.weekdays.indexOf(day);
-      if(pos===-1){
+    if (day) {
+      // TOCHECK Forbidden bitwise operation (no-bitwise)
+      day = day | 0;
+      let pos = this.user.reminder.weekdays.indexOf(day);
+      if (pos === -1) {
         this.user.reminder.weekdays.push(day);
-      }else{
-        do{
-          this.user.reminder.weekdays.splice(pos,1);
-          pos=this.user.reminder.weekdays.indexOf(day);
-        }while(pos>-1);
-        
-      }        
+      } else {
+        do {
+          this.user.reminder.weekdays.splice(pos, 1);
+          pos = this.user.reminder.weekdays.indexOf(day);
+        }while (pos > -1);
+
+      }
     }
-    if(time){
-      time=time|0;
-      this.user.reminder.time=time;  
+    if (time) {
+       // TOCHECK Forbidden bitwise operation (no-bitwise)
+      time = time | 0;
+      this.user.reminder.time = time;
     }
-    this.user.reminder.active=!!(this.user.reminder.weekdays.length);
+    this.user.reminder.active = !!(this.user.reminder.weekdays.length);
   }
 
   ngOnInit() {
-    this.locale=this.$i18n.locale;
+    this.locale = this.$i18n.locale;
   }
 
-  isChecked(day:number){
-    return (this.user.reminder.weekdays||[]).indexOf(day|0)>-1;
+  isChecked(day: number) {
+    return (this.user.reminder.weekdays || []).indexOf(day | 0) > -1;
   }
 
 
-  save(){
+  save() {
     this.$user.save(this.user).subscribe(
-      user=>{
-        this.$snack.open(this.$i18n.label().save_ok,this.$i18n.label().thanks,this.$i18n.snackOpt);
+      user => {
+        this.$snack.open(this.$i18n.label().save_ok, this.$i18n.label().thanks, this.$i18n.snackOpt);
       },
-      err=>(err)=>this.$snack.open(err.error,this.$i18n.label().thanks,this.$i18n.snackOpt)
+      // TOCHECK
+      // Parameter 'err' implicitly has an 'any' type, but a better type may be inferred from usage.ts(7044)
+      // Shadowed name: 'err' (no-shadowed-variable)tslint(1)
+
+      err => (err) => this.$snack.open(err.error, this.$i18n.label().thanks, this.$i18n.snackOpt)
     );
   }
 }
