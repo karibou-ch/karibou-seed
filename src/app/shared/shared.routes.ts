3c183a801bfcf9b67e5d4e09e02ec81c8fe4249b
olivier <olivier@evaletolab.ch>
2019-12-17 14:07:33 +0100

    new: routing strategy for multiple departement

--- src/app/shared/shared.routes.ts
+++ src/app/shared/shared.routes.ts
@@ -1,4 +1,4 @@
-import { Routes } from '@angular/router';
+import { Routes, Route } from '@angular/router';
 import { LoaderResolve } from 'kng2-core';
 
 import { KngHomeComponent } from '../kng-home/kng-home.component';
@@ -28,47 +28,26 @@ import { KngNavbarComponent } from '../kng-navbar';
 //   }
 // ];
 
+export const childrenRoute: Route[] = [
+  { path: 'products/:sku/:title', component: ProductComponent },
+  { path: 'products/:sku', component: ProductComponent },
+  { path: 'products', pathMatch: 'full', redirectTo: '/store/geneva/home' },
+  { path: 'category/:category/:child', component: ProductListComponent },
+  { path: 'category/:category', component: ProductListComponent },
+  { path: 'category', pathMatch: 'full', redirectTo: '' },
+  { path: '', pathMatch: 'full', redirectTo: '' }
+];
+
 export const appRoutes: Routes = [
+  { path: 'admin', loadChildren: '../kng-admin/admin.module#AdminModule' },
+  { path: 'cart', loadChildren: '../kng-cart/kng-cart.module#KngCartModule' },
+  { path: 'content', loadChildren: '../kng-document/kng-document.module#KngDocumentModule' },
   { path: 'me', loadChildren: '../kng-user/user.module#UserModule' },
   { path: 'shops', loadChildren: '../kng-shops/kng-shops.module#ShopsModule' },
-  { path: 'content', loadChildren: '../kng-document/kng-document.module#KngDocumentModule' },
-  { path: 'admin', loadChildren: '../kng-admin/admin.module#AdminModule' },
-  { path: 'cart', data: { target: 'cart' }, loadChildren: '../kng-cart/kng-cart.module#KngCartModule' },
   // FIXME path construction is ugly
-  {
-    path: 'home/cellar', component: KngHomeComponent, data: { target: 'cellar' }, resolve: { loader: LoaderResolve }, children: [
-      { path: 'products/:sku/:title', component: ProductComponent },
-      { path: 'products/:sku', component: ProductComponent },
-      { path: 'products', pathMatch: 'full', redirectTo: '/store/geneva/home/cellar' },
-      { path: 'category/:category/:child', component: ProductListComponent },
-      { path: 'category/:category', component: ProductListComponent },
-      { path: 'category', pathMatch: 'full', redirectTo: '' },
-      { path: '', pathMatch: 'full', redirectTo: '' }
-    ]
-  },
-
-  {
-    path: 'home/delicacy', component: KngHomeComponent, data: { target: 'delicacy' }, resolve: { loader: LoaderResolve }, children: [
-      { path: 'products/:sku/:title', component: ProductComponent },
-      { path: 'products/:sku', component: ProductComponent },
-      { path: 'products', pathMatch: 'full', redirectTo: '/store/geneva/home/delicacy' },
-      { path: 'category/:category/:child', component: ProductListComponent },
-      { path: 'category/:category', component: ProductListComponent },
-      { path: 'category', pathMatch: 'full', redirectTo: '' },
-      { path: '', pathMatch: 'full', redirectTo: '' }
-    ]
-  },
-
-  {
-    path: 'home', component: KngHomeComponent, data: { target: 'home' }, resolve: { loader: LoaderResolve }, children: [
-      { path: 'products/:sku/:title', component: ProductComponent },
-      { path: 'products/:sku', component: ProductComponent },
-      { path: 'products', pathMatch: 'full', redirectTo: '/store/geneva/home' },
-      { path: 'category/:category/:child', component: ProductListComponent },
-      { path: 'category/:category', component: ProductListComponent },
-      { path: 'category', pathMatch: 'full', redirectTo: '' },
-      { path: '', pathMatch: 'full', redirectTo: '' }
-    ]
-  },
-  // { path:'',pathMatch:'full', redirectTo:'home' },
+  { path: 'home', component: KngHomeComponent, data: { target: 'home' }, resolve: { loader: LoaderResolve }, children: childrenRoute},
+  { path: 'cellar', component: KngHomeComponent, data: { target: 'cellar' }, resolve: { loader: LoaderResolve }, children: childrenRoute},
+  { path: 'delicacy', component: KngHomeComponent, data: { target: 'delicacy' }, resolve: { loader: LoaderResolve }, children: childrenRoute},
+  { path: 'wellness', component: KngHomeComponent, data: { target: 'wellness' }, resolve: { loader: LoaderResolve }, children: childrenRoute},
+  { path: '', pathMatch: 'full', redirectTo: '' },
 ];
