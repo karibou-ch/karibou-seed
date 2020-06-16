b849a125dd4b592e0cc741dc10c065538faddee7
olivier <olivier@evaletolab.ch>
2020-06-16 11:10:06 +0200

    chg: new format for selected products

--- src/app/kng-product/product.component.ts
+++ src/app/kng-product/product.component.ts
@@ -34,9 +34,10 @@ import { map } from 'rxjs/operators';
 export class ProductComponent implements OnInit, OnDestroy {
   static WEEK_1: number = 86400 * 7;
 
-  @Input() sku: number;
+  @Input() sku: number;  
   @Input() config: any;
   @Input() categories: Category[];
+  @Input() selected: boolean;
   @Input() user: User = new User();
 
   @ViewChild('dialog', { static: true }) dialog: ElementRef;
