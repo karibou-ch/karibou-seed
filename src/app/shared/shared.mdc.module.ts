59868e0bdd463229c3cedec2b7a4032bcfb4209f
olivier <olivier@evaletolab.ch>
2021-12-27 12:39:16 +0100

    chg: remove unused deps

--- src/app/shared/shared.mdc.module.ts
+++ src/app/shared/shared.mdc.module.ts
@@ -3,23 +3,19 @@ import { NgModule } from '@angular/core';
 //
 // import material components
 import {
-  MdcCardModule,
   MdcChipsModule,
   MdcTextFieldModule,
   MdcCheckboxModule,
   MdcFormFieldModule,
   MdcDialogModule,
-  MdcMenuModule
 } from '@angular-mdc/web';
 
 
 @NgModule({
   exports: [
-    MdcCardModule,
     MdcChipsModule,
     MdcDialogModule,
     MdcFormFieldModule,
-    MdcMenuModule,
     MdcTextFieldModule,
     MdcCheckboxModule
     ],
