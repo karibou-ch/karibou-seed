8a10fd3582fcc58061cdef08364d91c8d1892197
olivier <olivier@evaletolab.ch>
2022-08-19 16:17:12 +0200

    new: home component

--- src/app/kng-root/kng-root.component.ts
+++ src/app/kng-root/kng-root.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { i18n, KngNavigationStateService } from '../common';
-import { ActivatedRoute } from '@angular/router';
-import { CartService, Product, ProductService } from 'kng2-core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CartService, Order, Product, ProductService, User } from 'kng2-core';
+import { LoaderService } from 'kng2-core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-kng-root',
@@ -13,36 +15,66 @@ export class KngRootComponent implements OnInit {
   config: any;
   currentShippingDay: Date;
   selected: Product[];
-  lockedHUB:boolean;
+  subscription:Subscription;
+  orders: Order[];
+  user: User;
+
   static SCROLL_CACHE = 0;
 
   constructor(
     public $i18n: i18n,
     public $cart: CartService,
+    public $loader: LoaderService,
     private $navigation: KngNavigationStateService,
     private $route: ActivatedRoute,
+    private $router: Router,
   ) {
     const loader = this.$route.snapshot.parent.data['loader'] || this.$route.snapshot.data['loader'];
     this.config = loader[0];
-
+    this.user = loader[1];
+    this.orders = loader[4] || [];
     this.currentShippingDay = new Date();
     this.selected = [];
+    this.subscription = new Subscription();
   }
 
 
   get locale() {
     return this.$i18n.locale;
   }
+
+  get label() {
+    return this.$i18n.label();
+  }
+
+  get lockedHUB() {
+    return this.$navigation.isLocked();
+  }
+  
+  get store() {
+    return this.$navigation.store;
+  }
+
+  set store(name) {
+    this.$navigation.store = name;
+  }
+
+
   
   ngOnInit() {
-    this.$route.params.subscribe(params => {
+    this.subscription.add(
+      this.$route.params.subscribe(params => {
       this.store = params['store'];
-      this.lockedHUB = !!this.config.shared.hub.domainOrigin;
+    }));
 
-      //
-      // Load products Selection
-      // this.loadProducts();
-    });
+    this.subscription.add(
+      this.$loader.update().subscribe(emit => {
+      if (!emit.config) {
+        return
+      }
+      this.config = emit.config;
+      this.store = this.config.shared.hub.slug;
+    }));
 
     //
     // FIXME check ugly hack !!
@@ -54,6 +86,9 @@ export class KngRootComponent implements OnInit {
     }, 100);
 
   }  
+  ngDestroy() {
+    this.subscription.unsubscribe();
+  }
 
   ngAfterViewChecked() {
   }
@@ -83,21 +118,13 @@ export class KngRootComponent implements OnInit {
     return product.photo.url + '/-/resize/300x/';
   }
 
-  doOpenMarket(slug) {
+  doOpenMarket(hub) {
     KngRootComponent.SCROLL_CACHE = 0;
+    this.$router.navigate(['/store',hub.slug,'home']);
   }
 
   doOpenShop(shop) {
-    console.log('---- on scroll',KngRootComponent.SCROLL_CACHE)
     KngRootComponent.SCROLL_CACHE = document.body.scrollTop;
   }
-
-  set store(name) {
-    this.$navigation.store = name;
-  }
-
-  get store() {
-    return this.$navigation.store;
-  }
-
+ 
 }
