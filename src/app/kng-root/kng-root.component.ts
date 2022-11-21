c23fda330fcee3cb6d489f0a5ebc8f113c6b3fa2
olivier <olivier@evaletolab.ch>
2022-11-21 12:15:51 +0100

    chg: debug message

--- src/app/kng-root/kng-root.component.ts
+++ src/app/kng-root/kng-root.component.ts
@@ -36,7 +36,7 @@ export class KngRootComponent implements OnInit {
     this.currentShippingDay = new Date();
     this.selected = [];
     this.subscription = new Subscription();
-    console.log('---- ROOT',this.orders)
+    console.log('---- ROOT component orders:',this.orders)
   }
 
 
