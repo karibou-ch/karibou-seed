6e32852db3a1d3c40edf04687a032e894b0560cd
olivier <olivier@evaletolab.ch>
2023-02-27 12:43:08 +0100

    fix:minimal import for home

--- src/app/common/common.mdc.module.ts
+++ src/app/common/common.mdc.module.ts
@@ -3,20 +3,14 @@ import { NgModule } from '@angular/core';
 //
 // import material components
 import {
-  MdcDrawerModule,
-  MdcListModule,
   MdcSnackbarModule,
-  MdcTabBarModule,
   MdcTopAppBarModule
 } from '@angular-mdc/web';
 
 
 @NgModule({
   exports: [
-    MdcDrawerModule,
-    MdcListModule,
     MdcSnackbarModule,
-    MdcTabBarModule,
     MdcTopAppBarModule
   ],
   declarations: []
