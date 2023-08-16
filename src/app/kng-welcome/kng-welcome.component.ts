630e6db1d89b834eaeab817ffff45632c0337177
olivier <olivier@evaletolab.ch>
2023-08-16 20:46:09 +0200

    new: design of landing

--- src/app/kng-welcome/kng-welcome.component.ts
+++ src/app/kng-welcome/kng-welcome.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation, HostListener, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, HostListener, ChangeDetectorRef, ViewChild, ElementRef } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 
@@ -17,10 +17,12 @@ import { EnumMetrics, MetricsService } from '../common/metrics.service';
 })
 export class KngWelcomeComponent implements OnInit {
 
+  @ViewChild('background') background: ElementRef;
+
   photos = [];
   exited: boolean;
 
-  K_BRAND = '/assets/img/k-brand-lg.png';
+  K_BRAND = '/assets/img/k-puce.svg';
   //
   // gradient of background image
   bgGradient = `linear-gradient(
@@ -31,13 +33,29 @@ export class KngWelcomeComponent implements OnInit {
 
   i18n: any = {
     fr: {
+      title_hypercenter: "Ville de Genève ",
+      title_periphery: "Communes avoisinantes ",
+      title_others: "Communes lointaines ",
+      title_none: "Nous ne livrons pas encore votre commune, essayez votre lieu de travail ?",
+      title_postal: "Entrez votre code postal pour afficher les tarifs de livraison",
+      title_discount: "➡️ Dès chf <b>__LIMIT__</b> d'achat: livraison"
     },
     en: {
+      title_hypercenter: "Geneva city ",
+      title_periphery: "Neighboring municipalities ",
+      title_others: "Distant municipalities ",
+      title_none: "We don't deliver to your municipality yet, why not try your workplace?",
+      title_postal: "Enter your postal code to view delivery rates",
+      title_discount: "➡️ From chf <b>__LIMIT__</b> of purchases: shipping "
     }
   };
 
   config: Config;
   postalCode: string;
+  image = new Image();
+  pi = 50;
+
+
 
   constructor(
     public $i18n: i18n,
@@ -53,10 +71,10 @@ export class KngWelcomeComponent implements OnInit {
     // Object.assign(this.config, loader[0]);
 
   
-    this.$photo.shops({active: true, random: 40}).subscribe((photos: any) => {
-      // remove underconstruction shops with missing photos //
-      this.photos = photos.filter(s => s.photo).map(shop => shop.photo.fg);
-    });
+    // this.$photo.shops({active: true, random: 40}).subscribe((photos: any) => {
+    //   // remove underconstruction shops with missing photos //
+    //   this.photos = photos.filter(s => s.photo).map(shop => shop.photo.fg);
+    // });
   }
 
   get locale() {
@@ -67,6 +85,112 @@ export class KngWelcomeComponent implements OnInit {
     return this.$i18n[this.locale][id];
   }
 
+  get faqs() {
+    return this.config.shared.faq || [];
+  }
+
+  get logo() {
+    return this.tagline.image;
+  }
+
+  get tagline() {
+    if (!this.config || !this.config.shared.tagLine) {
+      return {};
+    }
+    const shared = this.config.shared;
+    const hub = this.config.shared.hub;
+    return (hub && hub.name && shared.hub.domainOrigin) ? hub.tagLine : shared.tagLine;
+
+  }
+
+  get isMessageActive(){
+    if (!this.config || !this.config.shared.welcome) {
+      return false;
+    }
+    return !!this.config.shared.welcome.active
+  }
+
+  get welcomeMessage(){
+    if (!this.config || !this.config.shared.welcome) {
+      return "";
+    }
+    return this.config.shared.welcome.message[this.locale];
+  }
+
+  get about() {
+    if (!this.config || !this.config.shared.about) {
+      return {};
+    }
+    const shared = this.config.shared;
+    const hub =  this.config.shared.hub;
+    return (hub && hub.about && shared.hub.domainOrigin) ? hub.about : shared.about;
+  }
+
+
+
+  get shippingLabel() {
+    const code = this.shippingkeyCode;
+    if(!this.postalCode || this.postalCode.length<4) {
+      return "";
+    }
+
+    if(!code) {
+      return this.i18n[this.locale]['title_none'];
+    }
+
+    const label = 'title_'+code;
+    return this.i18n[this.locale][label];
+  }
+
+
+  get shippingPrice() {
+    const code = this.shippingkeyCode;
+    if(!code) {
+      return ""
+    }
+    return this.config.shared.shipping.price[code];
+  }
+
+  get isValidPostalCode() {
+    return this.getShippingPostalCode().indexOf(this.postalCode) > -1;
+  }
+
+  get shippingkeyCode() {
+    const code = ['hypercenter','periphery','others'].find(key => {
+      let codes = this.config.shared.shipping.district[key] || [];
+      return (codes.indexOf(this.postalCode) > -1)  
+    })
+    return code;
+  }
+
+
+
+
+  get width(){
+    return "100%";
+  }
+
+  get height(){
+    return "100%";
+  }
+
+  get canvas() {
+    return this.background.nativeElement;
+  }
+
+
+  get ctx() {
+    if(!this.canvas) {
+      return new CanvasRenderingContext2D();
+    }
+    return this.canvas.getContext('2d') as CanvasRenderingContext2D;
+  }  
+
+
+  get homeDestination() {
+    const market = this.store || 'artamis';
+    return ['/store',market,'home'];
+  }
 
   ngOnInit() {
     //
@@ -74,6 +198,8 @@ export class KngWelcomeComponent implements OnInit {
     // FIXME default HUB should not be Artamis!
     // window.location.host
     const hub = (this.config.shared.hub && this.config.shared.hub.slug)||'artamis';
+
+
 
     const metric ={
       path:window.location.pathname,
@@ -83,65 +209,68 @@ export class KngWelcomeComponent implements OnInit {
     };
     console.log('---DBG metrics',metric);
     this.$metric.event(EnumMetrics.metric_view_page,metric);
+    // const url = this.tagline.image;
+    // this.image.src = url;
+    // this.image.onload = () => {
+    //   this.drawBackground();
+    // }  
 
   }
   
+  drawBackground () {
+    const lum = Math.min(this.pi*0.3,70)|0;
+    const light = (Math.cos(this.pi * 0.1)+1)*10+50|0;
+    const angle = this.pi%360;
+    const saturation = `hsl(${angle},100%,${lum}%)`;
+    this.pi+=2;
+
+    
+    //
+    // cover image instead of stretch
+    // https://stackoverflow.com/a/66560970
+
+    this.ctx.drawImage(this.image, 0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.globalCompositeOperation = "multiply";
+    this.ctx.fillStyle = saturation;  // saturation at 100%
+    this.ctx.fillRect(0,0,this.canvas.width,this.canvas.height);  // apply the comp filter
+    this.ctx.globalCompositeOperation = "source-over";  // restore default comp    
+    setTimeout(()=>{
+      requestAnimationFrame(this.drawBackground.bind(this))
+    },80)
+  }   
 
   doLangSwitch() {
     this.$i18n.localeSwitch();
   }
 
-  getAbout(key) {
-    if (!this.config || !this.config.shared.about[key]) {
-      return;
+  getShippingDiscount(key:"A"|"B") {
+    const code = this.shippingkeyCode;
+    if(!code) {
+      return ""
     }
-    const shared = this.config.shared;
-    const hub = this.config.shared.hub;
-    return (hub && hub.name) ? hub.about[key][this.$i18n.locale] : shared.about[key][this.$i18n.locale];
-  }
 
-  getAboutImage() {
-    const defaultImg = (this.config && this.config.shared.hub && this.config.shared.hub.tagLine) ?
-          this.config.shared.hub.about.image : this.K_BRAND;
+  
 
-    const bgStyle = 'url(' + defaultImg + ')';
-    return {'background-image': this.bgGradient + bgStyle};
+    const label = this.i18n[this.locale].title_discount;
+    const limit = this.config.shared.shipping['discount'+key];
+    if(!limit) {
+      return "";
+    }
+    const price = (this.shippingPrice - this.config.shared.shipping['price'+key]);
+    if(!price) {
+      return "";
+    }
+    const label_price = price ? (price.toFixed(1)+' fr'):('OFFERTE')
+    return label.replace('__LIMIT__',limit)+' <b>'+label_price+' </b>';
   }
 
   getShippingPostalCode() {
     if (!this.config || !this.config.shared.shipping) {
       return [];
     }
-    const periphery = this.config.shared.shipping.periphery || [];
-    return [1201, 1202, 1203, 1204, 1205, 1206, 1207, 1208, 1209].concat(periphery);
-  }
-
-  getTagline(key) {
-    if (!this.config || !this.config.shared.tagLine[key]) {
-      return;
-    }
-    const shared = this.config.shared;
-    const hub = this.config.shared.hub;
-    return (hub && hub.name) ? hub.tagLine[key][this.$i18n.locale] : shared.tagLine[key][this.$i18n.locale];
-  }
-
-  getTaglineImage() {
-    if(!this.config.shared || !this.config.shared.tagLine) {
-      return {};
-    }
-    let defaultImg = this.config.shared.tagLine.image;
-    if(this.config.shared.hub && this.config.shared.hub.tagLine) {
-      defaultImg = this.config.shared.hub.tagLine.image;
-    }
-
-
-    const bgStyle = 'url(' + defaultImg + ')';
-    return {'background-image': bgStyle};
-
-  }
-
-  get isValidPostalCode() {
-    return this.getShippingPostalCode().indexOf(+this.postalCode) > -1;
+    const center = this.config.shared.shipping.district.hypercenter || [];
+    const periphery = this.config.shared.shipping.district.periphery || [];
+    return center.concat(periphery,this.config.shared.shipping.district.others);
   }
 
   isAppReady() {
@@ -156,7 +285,8 @@ export class KngWelcomeComponent implements OnInit {
     const href = target.getAttribute('href');
     //
     // verify if it's a routerLink
-    if(href && href.length > 2 && href.indexOf('http') === -1) {
+    if(href && href.length > 2 && 
+      (href.indexOf('http') === -1&&href.indexOf('mailto:') === -1&&href.indexOf('tel:') === -1)) {
       $event.stopPropagation();
       $event.preventDefault();
       this.exited = true;
