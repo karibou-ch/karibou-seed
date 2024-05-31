afb87dccab7528341de3a82a5fdf8326c8f5a010
olivier <olivier@evaletolab.ch>
2024-05-31 20:01:35 +0200

    new: add custom shipping days based on market setup

--- src/app/common/kng-subscription-option/kng-subscription-option.component.ts
+++ src/app/common/kng-subscription-option/kng-subscription-option.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { i18n } from '../i18n.service';
 import { 
   Config,
@@ -12,6 +12,7 @@ import {
   Hub, 
   CartAction
 } from 'kng2-core';
+import { Subscription } from 'rxjs';
 
 
 
@@ -20,8 +21,8 @@ import {
   templateUrl: './kng-subscription-option.component.html',
   styleUrls: ['./kng-subscription-option.component.scss']
 })
-export class KngSubscriptionOptionComponent implements OnInit {
-
+export class KngSubscriptionOptionComponent implements OnInit, OnDestroy {
+  private _subscription: Subscription;
   private _i18n = {
     fr:{
       title:'Décidez de la fréquence de livraison et du jour qui vous arrange pour les produits dans votre panier.',
@@ -47,11 +48,6 @@ export class KngSubscriptionOptionComponent implements OnInit {
     {label:{fr:"14 jours",en:"Biweekly"},id:"2weeks" },
     {label:{fr:"Mois", en:"month"},id:"month" },
   ]
-  dayOfWeek=[
-    {label:{fr:"Mardi",en:"Tuesday"},id:2},
-    {label:{fr:"Mercredi",en:"Wednesday"},id:3},
-    {label:{fr:"Vendredi",en:"Friday"},id:5}
-  ]
 
   config:Config;
   selIteration:any;
@@ -73,6 +69,7 @@ export class KngSubscriptionOptionComponent implements OnInit {
     private $loader: LoaderService,
     private $cdr: ChangeDetectorRef
   ) { 
+    this._subscription = new Subscription();
     this.items = [];
     this.subscriptions = [];
     this.subscriptionParams = {
@@ -88,10 +85,21 @@ export class KngSubscriptionOptionComponent implements OnInit {
 
     // const shippingDay = this.currentShippingDay();
     // const specialHours = ((shippingDay.getDay() == 6)? 12:16);
+  }
 
-}
+  get dayOfWeek() {
+    const weekdays = (this.hub && this.hub.weekdays)? this.hub.weekdays:[2,3,5];
+    const weekLabels = this.label.weekdays.split('_');
+    const label:any = {};
+    // label[this.locale]
+    const result = weekdays.map( day => ({label:weekLabels[day],id:day}));
+    return result;
+  }
 
   get nextShippingDay() {
+    if(!this.selDayOfWeek) {
+      return this.oneWeek[0];
+    }
     const day = this.selDayOfWeek.id;
     return this.oneWeek.find(date => date.getDay() == day);
   }
@@ -123,43 +131,53 @@ export class KngSubscriptionOptionComponent implements OnInit {
 
   }
 
+  ngOnDestroy() {
+    this._subscription.unsubscribe();
+  }
+
   async ngOnInit(){
+    this._subscription.add(
+      this.$loader.update().subscribe(emit => {
 
-
-    this.$loader.update().subscribe(emit => {
-
-      // ITEM_ADD       = 1,
-      // ITEM_REMOVE    = 2,
-      // ITEM_MAX       = 3,
-      // CART_INIT      = 4,
-      // CART_LOADED    = 5,
-      // CART_LOAD_ERROR= 6,
-      // CART_SAVE_ERROR= 7,
-      // CART_ADDRESS   = 8,
-      // CART_PAYMENT   = 9,
-      // CART_SHIPPING   =10,
-      if(emit.state && emit.state.action) {
-        //this.currentShippingDay = this.$cart.getCurrentShippingDay();
-        this.subscriptionParams = this.$cart.subscriptionGetParams();
-        this.selDayOfWeek = this.findDayOfWeek(this.subscriptionParams.dayOfWeek);
-        this.selIteration = this.findIteration(this.subscriptionParams.frequency);
-        this.$cdr.markForCheck();
-
-      }
-      if(!emit.config) {
-        return;
-      }
-      this.config = emit.config;
-      const times = Object.keys(this.config.shared.shipping.pricetime || {});
-      this.shippingtimes = times.reduce((shippingtimes,time)=> {
-        shippingtimes[time]=emit.config.shared.hub.shippingtimes[time];
-        return shippingtimes;
-      },{});  
-    });
+        // ITEM_ADD       = 1,
+        // ITEM_REMOVE    = 2,
+        // ITEM_MAX       = 3,
+        // CART_INIT      = 4,
+        // CART_LOADED    = 5,
+        // CART_LOAD_ERROR= 6,
+        // CART_SAVE_ERROR= 7,
+        // CART_ADDRESS   = 8,
+        // CART_PAYMENT   = 9,
+        // CART_SHIPPING   =10,
+        if(emit.state && emit.state.action) {
+          //this.currentShippingDay = this.$cart.getCurrentShippingDay();
+          console.log('---- ',emit.state.action,this.hub.name,this.hub.weekdays)
+          this.subscriptionParams = this.$cart.subscriptionGetParams();
+          this.selDayOfWeek = this.findDayOfWeek(this.subscriptionParams.dayOfWeek);
+          this.selIteration = this.findIteration(this.subscriptionParams.frequency);
+          this.$cdr.markForCheck();
+        }
+        if(!emit.config) {
+          return;
+        }
+        this.config = emit.config;
+        const times = Object.keys(this.config.shared.shipping.pricetime || {});
+        this.shippingtimes = times.reduce((shippingtimes,time)=> {
+          shippingtimes[time]=emit.config.shared.hub.shippingtimes[time];
+          return shippingtimes;
+        },{});  
+      })
+    );
   }
 
   findDayOfWeek(day:number) {
-    return this.dayOfWeek.find(dayOf => dayOf.id==day);
+    const find = this.dayOfWeek.find(dayOf => dayOf.id==day);
+    if(find) return find;
+    // FIXME this should be done in the change hub service
+    this.selDayOfWeek = this.dayOfWeek[0];
+    this.subscriptionParams.dayOfWeek = this.selDayOfWeek.id;
+    this.$cart.subscriptionSetParams(this.subscriptionParams);
+    return this.selDayOfWeek;
   }
 
   findIteration(iteration:string|CartItemFrequency) {
